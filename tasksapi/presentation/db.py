a6e1bfcffa4fd0f4f81671c2c28ca1439b70875b
Rikerdaves Alencar <rikerdaves.alencar@gmail.com>
2023-04-10 04:38:16 -0300

    delete

--- tasksapi/presentation/db.py
+++
@@ -1,6 +0,0 @@
-from pymongo import MongoClient
-
-client = MongoClient("mongodb+srv://ladydmc:devilmaycry5@apiserver.lcs3qz2.mongodb.net/?retryWrites=true&w=majority")
-db = client["apiserver"]
-
-
