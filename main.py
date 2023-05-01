fc8fb35a5c6c9f16ebc94a5248339fd82dbb0c82
Rikerdaves Alencar <rikerdaves.alencar@gmail.com>
2023-05-01 20:30:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ app = FastAPI()
 origins = ['http://localhost:5500',
            'http://127.0.0.1:5500',
            'https://127.0.0.1:8000',
-           'http://0.0.0.0:8000']
+           'http://0.0.0.0:8000',
+           'https://tarefasapi-u4ir.onrender.com']
 
 app.add_middleware(CORSMiddleware,
                    allow_origins=origins,
